9ea5fb353236ee2d8c27847c409beade4f3c5774
uuz <yyk@uuz.im>
2024-01-06 18:50:57 +0900

    读取文件名

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use fltk::{prelude::*, *};
 mod ui;
 mod text_file;
@@ -7,6 +9,19 @@ fn main() {
     let mut ui = ui::UserInterface::make_window();
     let mut window = ui.w1.clone();
     let mut br = ui.br;
+    //调用text_name_get函数，传入Path类型的变量
+    let mut file_names = text_file::text_name_get(Path::new("./"));
     
+    // Assuming `br` is a browser widget from the FLTK library
+   
+    for file_name in &file_names {
+        br.add(&file_name,false);
+    }
+
+    // Add the browser widget to the window
+    window.end();
+    window.show();
+    
+
     app.run().unwrap();
 }
